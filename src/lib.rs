efff112cd5619803e3239a8196745f270193e5bc
beetle <drquinsey@gmail.com>
2023-01-09 11:13:50 -0500

    made steps::omega correct and performant

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,17 @@
 /// This has the effect of dividing a number by 2 until it is odd.
 /// Odd numbers are simply returned.
 pub fn divide_while_even(n: u128) -> u128 {
-    n >> n.trailing_zeros()
+    match n {
+        0 => panic!("0 >> 0.trailing_zeros() always panics"),
+        _ => n >> n.trailing_zeros(),
+    }
 }
 
 /// Same as divide_while_even, but also returns how many times the number was divided by 2 before becoming odd.
 pub fn divide_while_even_and_trailing_zeros(n: u128) -> (u128, u32) {
+    if n == 0 {
+        panic!("0 >> 0.trailing_zeros() always panics");
+    }
     let zeros = n.trailing_zeros();    
     (n >> zeros, zeros)
 }
@@ -78,7 +84,7 @@ pub mod fall {
         // if n & 1 == 1 {
         //     omega_n_is_odd(n);
         // }
-
+        
         loop {
             let odd = n & 1 == 1;
 
@@ -126,7 +132,7 @@ pub mod steps {
         let mut steps = 0;
         while n != 1 {
             if n & 1 == 1 {
-                n = (3 * n) + 1;
+                n = 3 * n + 1;
                 steps += 1;
             }
             n /= 2;
@@ -136,30 +142,17 @@ pub mod steps {
     }
 
     /// Ideally far faster than steps::basic. Further testing needed.
-    pub fn omega(mut n: u128) -> u32 {
-        let mut steps = 0;
-        while n != 1 {
-            // See rules_super_speed for an explanation
-            let m = match n & 1 {
-                1 => 3 * n + 1,
-                _ => n,
-            };
-            let num_zeroes = m.trailing_zeros();
-            n = m / (1 << num_zeroes);
-            steps += num_zeroes;
-        }
-        steps
-
+    pub fn omega(n: u128) -> u32 {
         /*
         Big brain:
         If N is Even, simply make it odd!
         */
-        // if n & 1 != 1 {
-        //     omega_n_is_even(n)
-        // }
-        // else {
-        //     omega_n_is_odd(n)
-        // }
+        if n & 1 != 1 {
+            omega_n_is_even(n)
+        }
+        else {
+            omega_n_is_odd(n)
+        }
     }
 
 
@@ -177,7 +170,7 @@ pub mod steps {
             let m = 3 * n + 1;
             let zeros = m.trailing_zeros();
             n = m >> zeros;
-            steps += zeros;
+            steps += zeros + 1;
         }
         steps
     }
@@ -284,5 +277,36 @@ pub mod bouncy_numbers {
             }
         }
         retval
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Make sure the steps returned by steps::omega
+    #[test]
+    fn steps_range_conforms_to_oeis() {
+        // Number of steps to reach 1 for integers 1..=72
+        let oeis_steps: Vec<u32> = vec![0,1,7,2,5,8,16,3,19,6,14,9,9,17,17,4,12,20,20,7,
+        7,15,15,10,23,10,111,18,18,18,106,5,26,13,13,21,
+        21,21,34,8,109,8,29,16,16,16,104,11,24,24,24,11,
+        11,112,112,19,32,19,32,19,19,107,107,6,27,27,27,
+        14,14,14,102,22];
+
+        println!("OEIS STEPS HAS LENGTH {}", oeis_steps.len());
+
+        let r = 1..(oeis_steps.len() + 1);
+
+        let step_counts: Vec<u32> = r.clone().map(|n| steps::omega(n.try_into().unwrap())).collect();
+
+        println!("STEP COUNTS HAS LENGTH {}", step_counts.len());
+
+        for i in 0..oeis_steps.len() {
+            println!("{} => OEIS: {}, LIB: {}", i + 1, oeis_steps[i], step_counts[i]);
+            assert_eq!(oeis_steps[i], step_counts[i]);
+        }
     }
 }
