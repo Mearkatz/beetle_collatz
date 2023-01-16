55406ed6326c1f8a012fe2fb6c390b53d4f66ece
Mearkatz <drquinsey@gmail.com>
2023-01-15 19:28:07 -0500

    fixed some doc comments

--- src/steps_range.rs
+++ src/steps_range.rs
@@ -9,7 +9,7 @@ pub fn alpha(start: NonZeroU128, end: NonZeroU128) -> impl Iterator<Item = u32> 
     (start..end).map(|n| crate::steps::alpha(n.try_into().unwrap()))
 }
 
-/// Ideally much faster than steps_range::basic, by use of steps::omega instea of steps::basic.
+/// Ideally much faster than steps_range::alpha, by use of steps::omega instea of steps::alpha.
 ///
 /// Potentially less stable as a result, and may panic or overflow more often, I'm not sure yet.
 pub fn omega(start: NonZeroU128, end: NonZeroU128) -> impl Iterator<Item = u32> {
