1f92bb95605bc9f5b721398d5a5e3cdbf3cc77cc
Mearkatz <drquinsey@gmail.com>
2023-05-31 14:13:28 -0400

    replaced functions with traits

--- src/lib.rs
+++ src/lib.rs
@@ -3,80 +3,171 @@
 #![deny(missing_docs, unused_imports)]
 #![warn(clippy::unwrap_used)]
 
-use std::fmt::{Debug, Display};
+pub mod traits;
 
-use beetle_nonzero::NonZeroUnchecked;
-use no_panic::no_panic;
-use num::{FromPrimitive, Integer, PrimInt, Unsigned};
+mod impl_traits {
+    use std::ops::AddAssign;
 
-/// Types implementing this can be passed to the most if not all functions in this library
-pub trait Collatz: Unsigned + Integer + PrimInt + FromPrimitive + Debug + Display {}
+    use crate::traits::*;
+    use beetle_nonzero::{NonZero, RangeNonZeroUnsigned};
+    use num::One;
 
-impl<T> Collatz for T where T: Unsigned + Integer + PrimInt + FromPrimitive + Debug + Display {}
+    impl<T: Collatz> Rules for NonZero<T> {
+        fn odd_rule(self) -> Self {
+            let one = NonZero::one();
+            let three = one + one + one;
+            self * three + one
+        }
 
-/// This has the effect of dividing a number by 2 until it is odd.
-/// Odd numbers are simply returned.
-pub fn divide_while_even<T: Collatz>(n: NonZeroUnchecked<T>) -> Option<NonZeroUnchecked<T>> {
-    let n = n.value;
-    let trailing_zeros: usize = n.trailing_zeros().try_into().ok()?;
-    Some(NonZeroUnchecked::new(n >> trailing_zeros))
-}
+        fn even_rule(self) -> Self {
+            self >> 1
+        }
 
-/// Returns all the numbers N becomes on its way to falling to one.
-#[no_panic]
-pub fn transformations<T: Collatz>(n: NonZeroUnchecked<T>) -> Option<Vec<T>> {
-    let mut n = n;
-    let mut trans: Vec<T> = vec![n.value];
-    while !n.value.is_one() {
-        n = rules::basic(n)?;
-        trans.push(n.value);
+        fn rules(self) -> Self {
+            if self.get().is_even() {
+                self.even_rule()
+            } else {
+                self.odd_rule()
+            }
+        }
+
+        fn rules_halve_odds(self) -> Self {
+            if self.get().is_odd() {
+                self.odd_rule() >> 1
+            } else {
+                self >> 1
+            }
+        }
+
+        fn rules_remove_trailing_zeros(self) -> Option<Self> {
+            let partial_ans = if self.get().is_odd() {
+                self.odd_rule()
+            } else {
+                self
+            };
+            partial_ans.without_trailing_zeros()
+        }
     }
-    Some(trans)
+
+    impl<T: Collatz> Steps for NonZero<T> {
+        fn steps_to_one(mut self) -> Option<u64> {
+            let mut steps = 0;
+
+            while !self.is_one() {
+                self = self.rules();
+                steps += 1;
+            }
+            Some(steps)
+        }
+
+        fn steps_to_decrease(mut self) -> Option<u64> {
+            let mut steps = 0;
+            let starting_value = self;
+
+            while self > starting_value {
+                self = self.rules();
+                steps += 1;
+            }
+
+            Some(steps)
+        }
+
+        fn steps_to_one_for_even_number(self) -> Option<u64> {
+            let steps_to_become_odd: u64 = self.get().trailing_zeros().into();
+            let without_zeros = self.without_trailing_zeros()?;
+            Some(steps_to_become_odd + without_zeros.steps_to_one_for_odd_number()?)
+        }
+
+        fn steps_to_one_for_odd_number(mut self) -> Option<u64> {
+            let mut steps = 0;
+            while !self.is_one() {
+                // Number is known to be odd here,
+                // so apply odd rule,
+                self = self.odd_rule();
+                steps += 1;
+
+                // After the odd rule is applied, the resulting number is always even,
+                // so remove trailing zeros,
+                // and count each trailing zeros as a step
+                let tz = self.get().trailing_zeros();
+                self = self.without_trailing_zeros()?;
+                steps += tz as u64;
+            }
+            Some(steps)
+        }
+    }
+
+    // Impl for Primitive Integers (and BigInts apparently ??? What the fuck ???)
+    impl<T: Collatz> WithoutTrailingZeros for NonZero<T> {
+        type R = Option<Self>;
+        fn without_trailing_zeros(&self) -> Self::R {
+            let zeros: usize = self.get().trailing_zeros().try_into().ok()?;
+            Some(*self >> zeros)
+        }
+    }
+
+    impl<T: Collatz + Steps + AddAssign> Bouncy for NonZero<T> {
+        fn is_bouncy(&self) -> Option<bool> {
+            let steps: u64 = self.get().steps_to_one()?;
+            // let range: Range<T> = num::iter::range(T::zero(), value);
+            let range = RangeNonZeroUnsigned::new(NonZero::one(), *self);
+            for x in range {
+                let x_steps: u64 = x.steps_to_one()?;
+                if x_steps >= steps {
+                    return Some(false);
+                }
+            }
+            Some(true)
+        }
+    }
+
+    impl<T: Collatz + Rules> Transformations for NonZero<T> {
+        fn transformations_to_one(mut self) -> Option<Vec<Self>> {
+            let mut v = Vec::new();
+            while self.get().is_one() {
+                self = self.rules();
+                v.push(self);
+            }
+            Some(v)
+        }
+    }
 }
 
-pub mod bouncy_numbers;
-pub mod check_range;
-pub mod decrease_steps;
-pub mod fall;
-pub mod rules;
-pub mod steps;
-pub mod steps_range;
-
+#[allow(clippy::unwrap_used)]
 mod tests {
 
     // Number of steps to reach 1 for integers 1..=72 (according to the Online Encyclopedia of Integer Sequences)
     // For some reason this is counted as dead code currently, but it isn't dead code, I just convert it to a vec, or iter when using it.
     #[allow(dead_code)]
-    const OEIS_STEPS: [u32; 72] = [
+    const OEIS_STEPS: [u64; 72] = [
         0, 1, 7, 2, 5, 8, 16, 3, 19, 6, 14, 9, 9, 17, 17, 4, 12, 20, 20, 7, 7, 15, 15, 10, 23, 10,
         111, 18, 18, 18, 106, 5, 26, 13, 13, 21, 21, 21, 34, 8, 109, 8, 29, 16, 16, 16, 104, 11,
         24, 24, 24, 11, 11, 112, 112, 19, 32, 19, 32, 19, 19, 107, 107, 6, 27, 27, 27, 14, 14, 14,
         102, 22,
     ];
 
-    // Make sure the steps returned by steps::omega conforms with the OEIS STEPS
     #[test]
-    fn steps_module_works() {
-        use crate::NonZeroUnchecked;
-
-        // Unwrap here is fine, since if it panics during this test then there's a bug.
-        #[allow(clippy::unwrap_used)]
-        let step_counts: Vec<u32> = (1u32..=72u32)
-            .map(|n| crate::steps::omega(NonZeroUnchecked::new(n)).unwrap())
-            .collect();
-        assert_eq!(OEIS_STEPS.to_vec(), step_counts);
+    fn step_counts_for_integers_are_correct() {
+        use crate::traits::Steps;
+        use beetle_nonzero::NonZero;
+        use num::One;
+        let one: NonZero<u32> = NonZero::one();
+        assert_eq!(one.steps_to_one(), Some(0));
     }
 
     #[test]
-    fn steps_range_module_works() {
-        use crate::NonZeroUnchecked;
-        let start = NonZeroUnchecked::new(1u32);
-        let end = NonZeroUnchecked::new(73u32);
-        #[allow(clippy::unwrap_used)]
-        let step_counts: Vec<u32> = crate::steps_range::omega(start, end)
-            .map(|x| x.unwrap())
+    fn step_counts_for_ranges_are_correct() {
+        use crate::traits::Steps;
+        use beetle_nonzero::RangeNonZeroUnsigned;
+        let start: u32 = 1;
+        let stop: u32 = 73;
+        let steps: Vec<u64> = RangeNonZeroUnsigned::from_primitives(start, stop)
+            .expect("Failed to produce a RangeNonZeroUnsigned from two primitives")
+            .map(|n| {
+                n.steps_to_one()
+                    .expect("Failed to count how many steps a number took to fall")
+            })
             .collect();
-
-        assert_eq!(OEIS_STEPS.to_vec(), step_counts);
+        assert_eq!(steps, OEIS_STEPS.to_vec());
     }
 }
---
+++ src/traits.rs
@@ -0,0 +1,66 @@
+//! Traits related to the Collatz Conjecture and the rules of it
+
+use num::{Integer, PrimInt, Unsigned};
+
+/// Types implementing this can be passed to the most if not all functions in this library
+pub trait Collatz: Unsigned + Integer + PrimInt + From<u8> {}
+
+impl<T> Collatz for T where T: Unsigned + Integer + PrimInt + From<u8> {}
+
+/// Gives access to functions related to the rules of the Collatz Conjecture
+pub trait Rules: Sized {
+    /// Returns `3N + 1`.
+    fn odd_rule(self) -> Self;
+    /// Returns `N / 2`.
+    fn even_rule(self) -> Self;
+
+    /// If N is odd, this returns `3N + 1`, otherwise `N / 2`
+    fn rules(self) -> Self;
+
+    /// If N is odd, this returns `(3N + 1) / 2`, otherwise `N / 2`.    
+    fn rules_halve_odds(self) -> Self;
+
+    /// If N is odd, this returns `3N + 1`, otherwise `N (with any trailing zeros removed)`
+    fn rules_remove_trailing_zeros(self) -> Option<Self>;
+}
+
+/// Gives access to functions for counting the steps a number takes to reach 1,
+/// given the rules of the Collatz Conjecture
+pub trait Steps: Sized {
+    /// Returns how many steps a number takes to reach 1,
+    /// or None if there is an overflow
+    fn steps_to_one(self) -> Option<u64>;
+
+    /// Returns how many steps a number takes to decrease,
+    /// or None if there is an overflow
+    fn steps_to_decrease(self) -> Option<u64>;
+
+    /// Same as steps_to_one but faster for even numbers
+    fn steps_to_one_for_even_number(self) -> Option<u64>;
+
+    /// Same as steps_to_one but faster for odd numbers
+    fn steps_to_one_for_odd_number(self) -> Option<u64>;
+}
+
+/// Gives access to a function that returns a copy of a number without trailing zeros
+pub trait WithoutTrailingZeros: Sized {
+    /// Return type for `without_trailing_zeros`
+    type R;
+    /// Returns a copy of a number with its trailing zeros removed.
+    /// This also is effectively the same as dividing the number by 2 until it is odd.
+    fn without_trailing_zeros(&self) -> Self::R;
+}
+
+/// Gives access to a function that determines if a number if 'bouncy'.
+/// I made the term up, but I'm sure there's a proper term for it somewhere.
+pub trait Bouncy: Steps {
+    /// Returns whether this number takes more steps to fall to one than all the previous numbers.
+    /// Returns None if an overflow happens when calling `steps_to_one` on any integer along the way.
+    fn is_bouncy(&self) -> Option<bool>;
+}
+
+/// Gives access to a function that shows the transformations of a number as it falls to one
+pub trait Transformations: Sized {
+    /// Returns every number that this number becomes as it falls to one
+    fn transformations_to_one(self) -> Option<Vec<Self>>;
+}
